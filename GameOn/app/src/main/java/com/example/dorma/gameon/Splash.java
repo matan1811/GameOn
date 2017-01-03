1e68c662641ffb8c10f1e526c9b9f0a8df81ae96
matan harnoy <matan harnoy>
2017-01-03 14:27:17 +0200

    splash screen not working

---
+++ GameOn/app/src/main/java/com/example/dorma/gameon/Splash.java
@@ -0,0 +1,47 @@
+package com.example.dorma.gameon;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by dorma on 2017-01-03.
+ */
+
+public class Splash extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashh);
+
+        final ImageView iv = (ImageView) findViewById(R.id.gameonlogo);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(), R.anim.rotate);
+        final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                iv.setVisibility(View.GONE);
+                iv.startAnimation(an2);
+                finish();
+                Intent i = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(i);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
