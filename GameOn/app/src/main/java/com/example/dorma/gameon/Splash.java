3f692f8eeeaf42a39d3b691223883e08231d4d9e
matan harnoy <matan harnoy>
2017-01-04 20:12:15 +0200

    fixed image on splash screen

--- GameOn/app/src/main/java/com/example/dorma/gameon/Splash.java
+++ GameOn/app/src/main/java/com/example/dorma/gameon/Splash.java
@@ -3,6 +3,7 @@ package com.example.dorma.gameon;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,24 +13,17 @@ import android.widget.ImageView;
  * Created by dorma on 2017-01-03.
  */
 
-public class Splash extends Activity {
+public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splashh);
-        Intent i = new Intent(getBaseContext(), MainActivity.class);
-        startActivity(i);
-        /*final ImageView iv = (ImageView) findViewById(R.id.gamelogo);
+        final ImageView iv = (ImageView) findViewById(R.id.gamelogo);
         final Animation an2 = AnimationUtils.loadAnimation(getBaseContext(), R.anim.abc_fade_out);
 
         iv.startAnimation(an2);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
         Intent i = new Intent(getBaseContext(), MainActivity.class);
-        startActivity(i);*/
+        startActivity(i);
 
     }
 }
